fc8b5bc0bb709989ccc2d990858f07de2a40f67c
MerryM98 <61071116+MerryM98@users.noreply.github.com>
2020-02-28 20:55:05 -0500

    codificamos diferentes tipos de arreglos

--- index.php
+++ index.php
@@ -13,5 +13,48 @@ $arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"Merilian",'us
 echo("<br>");
 print_r($arreglo);
 
+echo("<br>...................");
+//tamaño de arreglo
+$tamano=count($arreglo);
+echo("<br>");
+echo("El tamaño de nuestro arreglo es: ".$tamano);
+echo("<br>...................");
+//bluces for
 
-?>
+for($centinela=0; $centinela<=10; $centinela++)
+{
+    echo(" <br>El valor de centinela es: ". $centinela."<br>" );
+}
+
+//bucle for para recorrer arreglos
+echo("<br>...................");
+$arreglo=array("Merilian","Meilyn","Mejia","Palacio");
+
+
+for($centinela=0;$centinela<count($arreglo);$centinela++)
+
+{
+    echo("<br>".$arreglo[$centinela]."<br>");
+}
+
+//bucle foreach para recorrer arreglos
+echo("<br>...................");
+$arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"Merilian",'usuario4'=>"Meilyn");
+foreach($arreglo as $clave=>$valor)
+{
+    echo"<br>"."la clave es: ".$clave."<br>";
+    echo "<br>".($valor)."<br>";
+}
+//bucle foreach para arreglo indexado
+echo("<br>...................");
+$arreglo=array("Merilian","Meilyn","Mejia","Palacio");
+
+foreach($arreglo as $clave=> $valor)
+{
+    echo "<br> El valor del cajón es: ".$valor;
+    echo "<br> La clave del cajon es : ".$clave;
+}
+
+
+
+?>  
